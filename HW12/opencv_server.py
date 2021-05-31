d778a72f74abf316f92277e8c8d4531fee8b0701
PowerLichen <dltm135@gmail.com>
2021-05-31 16:37:35 +0900

    Edit HW12-2 Server

--- HW12/opencv_server.py
+++ HW12/opencv_server.py
@@ -8,7 +8,7 @@
 
 import socket
 import cv2
-import numpy
+import numpy as np
 from queue import Queue
 from _thread import *
 SERVER_WEBCAM = 0
@@ -41,8 +41,8 @@ def video_chat_server(queue):
             continue
         encode_param=[int(cv2.IMWRITE_JPEG_QUALITY), 90]
         result, imgencode = cv2.imencode('.jpg', serv_frame, encode_param)
-        img_data = numpy.array(imgencode)
-        stringData = img_data.tobytes()
+        img_data = np.array(imgencode)
+        stringData = img_data.tostring()
         queue.put(stringData)
         cv2.imshow('Server:: Server_Video', serv_frame)
         key = cv2.waitKey(1)
@@ -53,7 +53,7 @@ def video_recvfrom_client(client_socket):
     while True:
         length = recvall(client_socket, 16)
         stringData = recvall(client_socket, int(length))
-        data = numpy.frombuffer(stringData, dtype='uint8')
+        data = np.frombuffer(stringData, dtype='uint8')
         decimg = cv2.imdecode(data, 1)
         cv2.imshow('Server::Received from Client', decimg)
         key = cv2.waitKey(1)
@@ -63,6 +63,7 @@ def video_recvfrom_client(client_socket):
 
 if __name__ == "__main__":
     enclosure_queue = Queue()
+    enclosure_queue2 = Queue()
     serverAddr = '127.0.0.1'
     PORT = 9999
     hostname = socket.gethostname()
@@ -72,12 +73,22 @@ if __name__ == "__main__":
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     server_socket.bind((serverAddr, PORT))
-    server_socket.listen()
+    server_socket.listen(1)
     print('Server::Video chatting server started')
     start_new_thread(video_chat_server, (enclosure_queue,))
     print('Server::Waitint for client .... ')
     client_socket, addr = server_socket.accept()
     print('Server::connected to ({} : {})'.format(client_socket, addr))
     start_new_thread(video_sendto_client, (client_socket, addr, enclosure_queue,))
-    start_new_thread(video_recvfrom_client, (client_socket,))
+
+    while True:
+        length = recvall(client_socket, 16)
+        stringData = recvall(client_socket, int(length))
+        data = np.frombuffer(stringData, dtype='uint8')
+        decimg = cv2.imdecode(data, 1)
+        cv2.imshow('Server::Received from Client', decimg)
+        key = cv2.waitKey(1)
+        if key == 27:
+            break
+    
     server_socket.close()
